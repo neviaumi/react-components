b9752103b764b65006c6af42db2da07c343d20e4
David Ng <david.ng.dev@gmail.com>
2023-03-31 12:10:46 +0100

    ISSUE-001: include github action

---
+++ scripts/ci/publish.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -ex
+
+export HUSKY=0
+# https://github.com/orgs/community/discussions/26560
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config user.name "github-actions[bot]"
+echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > ~/.npmrc
+CURRENT_WORKING_DIRECTORY=$(pwd)
+
+if [ -n "$PREFERRED_VERSION" ]; then
+  echo "Use version: $PREFERRED_VERSION"
+  VERSION=$PREFERRED_VERSION
+else
+  echo "Use date version"
+  if [ "$GITHUB_REF_NAME" == "development" ]; then
+    echo "Use date version on development"
+    VERSION=$(date +"%Y.%-m.%-d-alpha.$(($(date +"%-H") + 1))%M")
+  else
+    echo "Use date version on main"
+    VERSION=$(date +'%Y.%-m.%-d')
+  fi
+fi
+export RELEASE_BRANCH="release-$VERSION"
+COMMIT_MESSAGE="publish v$VERSION [skip ci]"
+git switch -c "$RELEASE_BRANCH"
+
+npm version -m "$COMMIT_MESSAGE" $VERSION
+npm build
+npm publish
+git push --set-upstream origin "$RELEASE_BRANCH"
+export RELEASE_VERSION=$VERSION
