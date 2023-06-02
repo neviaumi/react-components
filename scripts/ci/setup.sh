34db6047ab5440b9b714308ba84d623de8e31fab
David Ng <david.ng.dev@gmail.com>
2023-06-02 11:40:03 +0000

    ISSUE-167: include devcontainer config

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -1,6 +1,21 @@
 #! /usr/bin/env bash
 
-CURRENT_BRANCH=$(git branch --show-current)
-echo "Current branch is $CURRENT_BRANCH"
+set -e
 
-npm ci
+CURRENT_BRANCH=$(git branch --show-current)
+NVM_SOURCE="$NVM_DIR/nvm.sh"
+echo "Current branch is $CURRENT_BRANCH"
+if [ -f "$NVM_SOURCE" ]
+then
+  source "$NVM_DIR/nvm.sh"
+  nvm install
+fi
+
+if [ -z "$CI" ]
+then
+  echo "Not in CI, installing dependencies with npm install"
+  npm install
+else
+  echo "Run in CI, installing dependencies with npm ci"
+  npm ci
+fi
