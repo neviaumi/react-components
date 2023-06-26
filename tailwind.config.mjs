e8eae06422cf5e6ca3543de522f3d8e08133aadd
David Ng <david.ng.dev@gmail.com>
2023-06-26 19:35:25 +0100

    ISSUE-239: replace tailwind.config.cjs with .mjs
    this close #239

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,48 @@
-import myTailwindConfig from './tailwind.config.cjs';
+// import myTailwindConfig from './tailwind.config.cjs';
+//
+// const config = myTailwindConfig;
+import tailwindConfig from '@busybox/tailwindcss-config/dist/tailwind.config.mjs';
+import { assocPath, path, pipe } from 'ramda';
+import colors from 'tailwindcss/colors.js';
 
-const config = myTailwindConfig;
+function extractColor(colorPath) {
+  return assocPath(colorPath, path(colorPath, colors));
+}
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './cypress/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+    './.storybook/**/*.{.js,cjs}',
+  ],
+  presets: [tailwindConfig],
+  theme: {
+    extend: {
+      backgroundColor: ({ theme }) => {
+        return pipe(
+          assocPath(['disabled'], {
+            DEFAULT: theme('colors.gray.200'),
+            hover: theme('colors.gray.50'),
+          }),
+        )(theme('colors'));
+      },
+      colors: pipe(
+        extractColor(['gray', '50']),
+        extractColor(['gray', '200']),
+        extractColor(['gray', '600']),
+        extractColor(['gray', '800']),
+      )({}),
+      textColor: ({ theme }) => {
+        return pipe(
+          assocPath(['disabled'], {
+            DEFAULT: theme('colors.gray.800'),
+            hover: theme('colors.gray.600'),
+          }),
+        )(theme('colors'));
+      },
+    },
+  },
+};
 
 export default config;
