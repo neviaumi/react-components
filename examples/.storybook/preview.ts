96888baae70328c257e24d38a7255ce4b4896e80
David Ng <david.ng.dev@gmail.com>
2024-01-13 01:12:57 +0000

    GH-793: remove unwanted packages
    this close #793

--- examples/.storybook/preview.ts
+++ examples/.storybook/preview.ts
@@ -1,10 +1,8 @@
 import './preview.css';
 
-// import { withConsole } from '@storybook/addon-console';
 import { type Preview } from '@storybook/react';
 
 const preview: Preview = {
-  // decorators: [(storyFn, context) => withConsole()(storyFn)(context)],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
